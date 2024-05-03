c444922713b982c917921b7b41631a84b35c77ca
Robeshan <cbrrobeshan04@gmail.com>
2024-05-03 11:28:01 +0530

    New Files Added

--- LOGIN SIGNIN/LOGIN SIGNIN/php/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
- 
- $con = mysqli_connect("localhost","root","","sam") or die("Couldn't connect");
-
-?>
--- LOGIN SIGNIN/LOGIN SIGNIN/sql/sam.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users(
-    Id int PRIMARY KEY AUTO_INCREMENT,
-    Username varchar(255),
-    Email varchar(255),
-    Password varchar(255)
-);
